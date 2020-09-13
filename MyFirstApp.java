189e442fd1ce016c7da523124537e235f4c4eeca
Lena <elenachepikova28@gmail.com>
2020-09-13 11:37:15 +0300

    добавлены строки для дальнейшего удаления

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,5 +1,7 @@
 public class MyFirstApp {
 	public static void main(String[] args) {
 	System.out.println("Привет");
+// 	System.out.println("Потом удалить");
+// 	System.out.println("И это тоже удалить");
 	}
 }
